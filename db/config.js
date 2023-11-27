14a3251726af7a34907bc6fcf332ce7213c46e66
gabnbar <gabitaewang@gmail.com>
2023-11-27 14:45:12 +0000

    create export new connection pool

---
+++ db/config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  database: process.env.PGDATABASE || "default_database_name",
+  // Other configuration variables can be added here
+};
--- db/connection.js
+++ db/connection.js
@@ -2,3 +2,12 @@
  * Create your connection to the DB in this file
  * and remember to export it
  */
+const { Pool } = require("pg");
+
+if (!process.env.PGDATABASE) {
+  throw new Error("No PGDATABASE configured");
+}
+
+const pool = new Pool();
+
+module.exports = pool;
